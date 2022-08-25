6d53c984df98b44e6dbf23619d50f9ad45af2cee
joao <njoao99@gmail.com>
2022-08-25 20:28:45 +0100

    iteration 1

---
+++ main.js
@@ -0,0 +1,18 @@
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+
+const width = canvas.width;
+const height = canvas.height;
+
+// Iteration 1
+function drawGrid() {
+  // TODO: write the code of the function
+}
+
+function drawEverything() {
+  drawGrid();
+  // drawPlayer()
+  // drawTreasure()
+}
+
+drawEverything();
